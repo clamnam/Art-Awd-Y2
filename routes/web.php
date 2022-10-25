d91dad4fe330aefa0008e4c735028bfe08fc02d0
clamnam <97921636+clamnam@users.noreply.github.com>
2022-10-25 17:15:31 +0100

    error correction

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('/arts',ArtController::class)->middleware(['auth']);
+Route::resource('/art',ArtController::class)->middleware(['auth']);
