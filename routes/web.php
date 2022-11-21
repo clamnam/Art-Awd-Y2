531675ac27ab9f7b6773e53acc1652b692defd68
clamnam <97921636+clamnam@users.noreply.github.com>
2022-11-21 18:16:01 +0000

    uuid removed seeding working apart from image

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtController;
+use App\Http\Controllers\user\ArtController;
+use App\Http\Controllers\\ArtController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
