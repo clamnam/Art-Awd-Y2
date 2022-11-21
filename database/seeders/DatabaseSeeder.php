65afdd2b5c1c7b9a60ec4a274a3bee9a0e4a4adf
clamnam <97921636+clamnam@users.noreply.github.com>
2022-11-21 14:14:55 +0000

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // $this->call(ArtSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
--- resources/views/arts/show.blade.php
+++ resources/views/arts/show.blade.php
@@ -12,7 +12,7 @@
                 <x-alert-success>
                     {{ session('success') }}
                 </x-alert-success>
-        </div>
+            </div>
             <div class="flex">
 
             </div>
@@ -32,24 +32,29 @@
                     <p class="mt-4 whitespace-pre-wrap"{{ $art->text }}></p>
                     <div class="">
                         {{-- brings images in using file names stored in the db, that are stored in the path in asset --}}
-                    <tr>
-                        <td rowspan="6">
-                            <img src="{{asset('storage/images/' . $art->art_image)  }}" width="150" alt="">
-                        </td>
-                    </tr>
-                </div>
+                        <tr>
+                            <td rowspan="6">
+                                <img src="{{ asset('storage/images/' . $art->art_image) }}" width="150"
+                                    alt="">
+                            </td>
+                        </tr>
+                    </div>
                 </div>
 
 
             </div>
-            <a class="items-end btn-link" href="{{ route('arts.edit', $art) }}" >Edit art</a>
-            <form action="{{ route('arts.destroy', $art) }}" method="post">
-                @method('delete')
-            @csrf
-            {{-- Allows you to delete with an "are you sure?" prompt --}}
-            <button type="submit" class="btn btn-danger ml-4" onclick="return confirm('Are you sure you wish to delete this note?')">Delete piece</button>
-        </form>
-                            {{-- diff for humans changes the db format of date to a more readable format --}}
+            <div class="flex">
+                <a class="items-end btn-link" href="{{ route('arts.edit', $art) }}">Edit art</a>
+
+                <form action="{{ route('arts.destroy', $art) }}" method="post">
+                    @method('delete')
+                    @csrf
+                    {{-- Allows you to delete with an "are you sure?" prompt --}}
+                    <button type="submit" class="btn btn-danger ml-4"
+                        onclick="return confirm('Are you sure you wish to delete this art?')">Delete piece</button>
+                </form>
+            </div>
+            {{-- diff for humans changes the db format of date to a more readable format --}}
 
             <p class="opacity-70 ml-8"><strong>Created: </strong> {{ $art->created_at->diffForHumans() }}</p>
             <p class="opacity-70 ml-8"><strong>Updated at: </strong> {{ $art->updated_at->diffForHumans() }}</p>
