28b45ac6f577ae038a764694fffac20efd1d54d4
clamnam <97921636+clamnam@users.noreply.github.com>
2022-11-21 15:35:57 +0000

    Revert "."
    This reverts commit a24aa9e71481d42131f285be5c0c21e260ca50bb.

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::get('/', function () {
 
 //Route::resource('/art',ArtController::class)->middleware(['auth']);
 
-Route::resource('/arts', ArtController::class)->middleware(['auth']);
+Route::resource('/arts',ArtController::class)->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
